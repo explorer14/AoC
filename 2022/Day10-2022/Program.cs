c536d5cb748bc7ca59f87448774a2fa285c25690
explorer14 <amanag1983@gmail.com>
2022-12-12 22:27:55 +0100

    Day 10 Part 1 done

--- 2022/Day10-2022/Program.cs
+++ 2022/Day10-2022/Program.cs
@@ -1,1 +1,49 @@
-﻿var lines = File.ReadAllLines(@"C:\Users\aman-agrawal\Code\AoC\2022\Day10-2022\Input.txt");
+﻿var lines = File.ReadAllLines(@"C:\Users\aman-agrawal\Code\AoC\2022\Day10-2022\Input-Large.txt");
+
+var x = 1;
+var cycle = 0;
+var totalSignalStrength = 0;
+
+foreach (var line in lines)
+{
+    if (line == "noop")
+    {
+        ++cycle;
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+    }
+
+    if (line.StartsWith("addx"))
+    {
+        ++cycle;
+        var add = int.Parse(line.Substring(5, line.Length - 5));
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+
+        ++cycle;
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+
+        x += add;
+    }
+}
+
+Console.WriteLine(totalSignalStrength);
