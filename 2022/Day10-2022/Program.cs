5276a77750a2ffd576d37d0d86fb548fbb522c0f
explorer14 <amanag1983@gmail.com>
2022-12-12 22:45:52 +0100

    Start with part 2

--- 2022/Day10-2022/Program.cs
+++ 2022/Day10-2022/Program.cs
@@ -46,4 +46,47 @@ foreach (var line in lines)
     }
 }
 
+// Part 2
+foreach (var line in lines)
+{
+    if (line == "noop")
+    {
+        ++cycle;
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+    }
+
+    if (line.StartsWith("addx"))
+    {
+        ++cycle;
+        var add = int.Parse(line.Substring(5, line.Length - 5));
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+
+        ++cycle;
+
+        if (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220)
+            totalSignalStrength += cycle * x;
+
+        x += add;
+    }
+}
+
 Console.WriteLine(totalSignalStrength);
