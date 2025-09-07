6435e0a70a29611a6a2d6995b2b93e0be052f1e3
explorer14 <amanag1983@gmail.com>
2025-09-07 18:24:57 +0200

    dos-donts are annoying

--- 2024/2024.Tests/Day3.cs
+++ 2024/2024.Tests/Day3.cs
@@ -14,32 +14,81 @@ public class Day3(ITestOutputHelper testOutputHelper)
     {
         var memory = File.ReadAllText(inputFileName);
         var timer = Stopwatch.StartNew();
-        var result = ProcessMemory(memory);
+        var result = ProcessMemory2(memory);
         timer.Stop();
         testOutputHelper.WriteLine($"Answer {result} in {timer.Elapsed.TotalMilliseconds} ms");
     }
 
-    private int ProcessMemory(string memory)
+    // private int ProcessMemory(string memory)
+    // {
+    //     var pattern = @"mul\(\d*,\d*\)";
+    //     var matches = Regex.Matches(memory, pattern);
+    //     
+    //     var result = 0;
+    //
+    //     foreach (Match match in matches)
+    //     {
+    //         result += Exec(match.Value);            
+    //     }
+    //
+    //     return result;
+    // }
+
+    private int ProcessMemory2(string memory)
     {
-        var pattern = @"mul\(\d*,\d*\)";
-        var matches = Regex.Matches(memory, pattern);
+        var instructionBuffer = string.Empty;
         var result = 0;
 
-        foreach (Match match in matches)
+        foreach (var c in memory)
         {
-            result += Exec(match.Value);            
-        }
+            instructionBuffer += c;
+            var isMulEnabled = IsMulEnabled(instructionBuffer);
+            var (isMulInstruction, mulInstructions) = ContainsMulInstruction(instructionBuffer);
 
+            if (isMulInstruction && isMulEnabled)
+            {
+                result += Exec(mulInstructions);
+                instructionBuffer = string.Empty;
+            }
+        }
         return result;
     }
 
-    private int Exec(string mulInstruction)
+    private (bool, string[]) ContainsMulInstruction(string instructionBuffer)
     {
-        var csvNums = mulInstruction[4..^1]
-            .Split(",", StringSplitOptions.RemoveEmptyEntries)
-            .Select(int.Parse)
-            .ToList();
+        var pattern = @"mul\(\d*,\d*\)";
+        var matches = Regex.Matches(instructionBuffer, pattern);
+        return (matches.Count > 0, matches.Select(m => m.Value).ToArray());
+    }
+
+    // find a way to determine when the flag should flip
+    // do -> don't => disabled
+    // don't -> do => enabled
+    // do -> do => enabled
+    // don't -> don't => disabled
+    private bool IsMulEnabled(string instructionBuffer)
+    {
+        var disablePattern = @"don't\(\)";
+        var enablePattern = @"do\(\)";
+        var disableMatch = Regex.Match(instructionBuffer, disablePattern);
+        var enableMatch = Regex.Match(instructionBuffer, enablePattern);
         
-        return csvNums[0] * csvNums[1];
+        return disableMatch.Success | enableMatch.Success;
+    }
+
+    private int Exec(string[] mulInstructions)
+    {
+        var result = 0;
+        foreach (var mulInstruction in mulInstructions)
+        {
+            var csvNums = mulInstruction[4..^1]
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        
+            result += csvNums[0] * csvNums[1];    
+        }
+
+        return result;
     }
 }
